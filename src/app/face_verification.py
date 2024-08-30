c18ee2a8f381cfdaddd29b2f0cdec86f3604a52c
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-08-30 19:14:19 +0300

    Рефакторинг тестов и изменение flake ignore

--- src/app/face_verification.py
+++ src/app/face_verification.py
@@ -5,10 +5,12 @@ from sqlalchemy.orm import Session
 import logging
 logging.basicConfig(level=logging.INFO)
 
+
 class FaceVerification:
     """Класс обработки фотографии."""
 
     def __init__(self, db: Session):
+        """Инициализация класса верификации."""
         self.db = db
 
     def embedings_vector(self, img_path: str) -> list[float]:
@@ -20,8 +22,8 @@ class FaceVerification:
         """Верификация пользователя."""
         try:
             vector = self.embedings_vector(img_path=img_path)
-        except Exception as e:
-            logging.error(f'Ошибка при создании вектора: {e}')
+        except Exception as verify_error:
+            logging.error(f'Ошибка при создании вектора: {verify_error}')
             return False
 
         verified = self.check_user(user_id, vector)
@@ -48,13 +50,13 @@ class FaceVerification:
                     user_id=user_id,
                     vector=embedding_vector,
                     created_at=datetime.now(),
-                    updated_at=datetime.now()
+                    updated_at=datetime.now(),
                 )
                 self.db.add(new_user_face_data)
                 self.db.commit()
                 verified = True
 
-        except Exception as e:
-            logging.error(f'Ошибка при доступе к базе данных: {e}')
+        except Exception as error_db:
+            logging.error(f'Ошибка при доступе к базе данных: {error_db}')
 
         return verified
--- src/tests/test_consumer.py
+++ src/tests/test_consumer.py
@@ -1,6 +1,6 @@
 import pytest
 import json
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock
 from app.consumer import Consumer
 import logging
 
@@ -15,8 +15,9 @@ def mock_kafka_consumer(mocker):
 def mock_face_verification(mocker):
     """Фикстура для мока FaceVerification."""
     mock_verification = mocker.patch('app.consumer.FaceVerification')
-    mock_verification.return_value.check_user.return_value = True
-    return mock_verification
+    mock_verification_instance = mock_verification.return_value
+    mock_verification_instance.verify = MagicMock(return_value=True)
+    return mock_verification_instance
 
 @pytest.mark.asyncio
 async def test_consumer_process_message_logs(caplog, mock_kafka_consumer, mock_face_verification):
@@ -25,20 +26,12 @@ async def test_consumer_process_message_logs(caplog, mock_kafka_consumer, mock_f
         "photo_path": "test_photo.jpg",
         "user_id": 1
     }
-
     consumer = Consumer()
-
     mock_kafka_consumer.return_value.__aiter__.return_value = [
         AsyncMock(value=json.dumps(test_message).encode('utf-8'))
     ]
-
-    mock_face_verification_instance = mock_face_verification.return_value
-
     with caplog.at_level(logging.INFO):
         await consumer.start()
-
     assert "Получено сообщение" in caplog.text
     assert "Извлечённые данные - img_path: test_photo.jpg, user_id: 1" in caplog.text
-
-    mock_face_verification.assert_called_once()
-    mock_face_verification_instance.embedings_vector.assert_called_once_with("test_photo.jpg")
+    mock_face_verification.verify.assert_called_once_with(1, "test_photo.jpg")
