75aa284d68c8fbf992f3a5671fa17fc689b5152f
Irina Ivashko <ira.ivashko.99@gmail.com>
2024-09-02 10:55:50 +0000

    обновление чартов

--- src/app/models.py
+++ src/app/models.py
@@ -1,5 +1,9 @@
 from sqlalchemy import Column, Integer, TIMESTAMP, ForeignKey, Table, ARRAY, Numeric, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import registry
+from app.database import engine
+
+mapper_registry = registry()
 
 metadata = MetaData(schema="ivashko_schema")
 Base = declarative_base(metadata=metadata)
@@ -13,3 +17,9 @@ class UserFaceData(Base):
     created_at = Column(TIMESTAMP, default=None)
     updated_at = Column(TIMESTAMP, default=None)
 
+users_table = Table('users_ivashko', Base.metadata, autoload_with=engine, schema='ivashko_schema')
+
+class Users:
+    pass
+
+mapper_registry.map_imperatively(Users, users_table)
